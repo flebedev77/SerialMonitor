8e0b52e7c75c34fa04a29ad57cc3ec24c72c901f
flebedev77 <flebedev99@gmail.com>
2024-07-07 10:19:58 +0930

    Changed the serial code

--- src/script.js
+++ src/script.js
@@ -1,34 +1,48 @@
 const output = document.getElementById("output");
 
 document.getElementById("start-button").addEventListener("click", () => {
-    navigator.serial
-        .requestPort()
-        .then(port => {
-            readPort(port);
-        }).catch(e => {
-            alert("You need to select a port");
-        })
+    listenForSerialDevice();
 })
-
-async function readPort(port) {
-    while (port.readable) {
-        const reader = port.readable.getReader();
+        
+async function listenForSerialDevice() {
+    var port, textEncoder, writableStreamClosed, writer, historyIndex = -1;
+    const lineHistory = [];
+    async function connectSerial() {
         try {
-            while (true) {
-                const { value, done } = await reader.read();
-                if (done) {
-                    // |reader| has been canceled.
-                    break;
-                }
+            // Prompt user to select any serial port.
+            port = await navigator.serial.requestPort();
+            await port.open({ baudRate: 115200 });
+            let settings = {};
 
-                output.innerText += "\n" + value;
-            }
-        } catch (error) {
-            console.error(error);
-        } finally {
-            reader.releaseLock();
+            textEncoder = new TextEncoderStream();
+            writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+            writer = textEncoder.writable.getWriter();
+            await listenToPort();
+        } catch (e) {
+            alert("Serial Connection Failed" + e);
         }
     }
+    async function listenToPort() {
+        const textDecoder = new TextDecoderStream();
+        const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+        const reader = textDecoder.readable.getReader();
 
+        // Listen to data coming from the serial device.
+        while (true) {
+            const { value, done } = await reader.read();
+            if (done) {
+                // Allow the serial port to be closed later.
+                console.log('[readLoop] DONE', done);
+                reader.releaseLock();
+                break;
+            }
+            // value is a string.
+            parseData(value);
+        }
+    }
+    await connectSerial();
+}
 
+function parseData(value) {
+    output.innerText += "\n " + value;
 }
